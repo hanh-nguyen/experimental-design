081e04974e2b622113fde0dfb017680e1780605d
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2020-06-06 16:28:04 -0400

    add class and pytest tests

---
+++ dev/abtesting.py
@@ -0,0 +1,80 @@
+from scipy import stats
+
+
+class AnalyticTesting:
+    def __init__(self, test_conv, con_conv, ci=0.95):
+        self.test_conv = test_conv
+        self.con_conv = con_conv
+        self.lift = self.test_conv - self.con_conv
+        self.ci = ci
+        self._z_score = stats.norm.isf((1 - self.ci) / 2)
+
+    def update_sizes(
+        self, total_size=None, con_ratio=None, test_size=None, con_size=None
+    ):
+        if (total_size != None) & (con_ratio != None):
+            self.test_size = round(total_size * (1 - con_ratio))
+            self.con_size = round(total_size * con_ratio)
+        else:
+            self.test_size = test_size
+            self.con_size = con_size
+        self._sd_test = (
+            (self.test_conv * (1 - self.test_conv)) / self.test_size
+        ) ** 0.5
+        self._sd_con = ((self.con_conv * (1 - self.con_conv)) / self.con_size) ** 0.5
+
+    def update_ci(self, new_ci):
+        self.ci = new_ci
+        self._z_score = stats.norm.isf((1 - self.ci) / 2)
+
+    def _get_sd(self):
+        """
+        Calculate the standard deviation of the lift
+        """
+        self._sd = (
+            (self.test_conv * (1 - self.test_conv)) / self.test_size
+            + (self.con_conv * (1 - self.con_conv)) / self.con_size
+        ) ** 0.5
+        return self._sd
+
+    def get_ci(self):
+        """
+        Calculate the confidence interval of the lift
+        Assumptions: lift > 0, or test ratio > control ratio
+
+        Returns:
+        Lower bound and upper bound of the lift
+        """
+        lwr_bnd = self.lift - self._sd * self._z_score
+        upr_bnd = self.lift + self._sd * self._z_score
+        return lwr_bnd, upr_bnd
+
+    def get_pvalue(self):
+        """
+        Calculate p-value of the lift between two ratios given their sample sizes
+        """
+        lift = -abs(self.test_conv - self.con_conv)
+        p_value = 2 * stats.norm.cdf(lift, loc=0, scale=self._get_sd())
+        return p_value
+
+    def get_ci_test(self):
+        """
+        Calculate the confidence interval of the test conversion rate
+        
+        Returns:
+        Lower bound and upper bound of the lift
+        """
+        lwr_bnd = self.test_conv - self._sd_test * self._z_score
+        upr_bnd = self.test_conv + self._sd_test * self._z_score
+        return lwr_bnd, upr_bnd
+
+    def get_ci_con(self):
+        """
+        Calculate the confidence interval of the test conversion rate
+        
+        Returns:
+        Lower bound and upper bound of the lift
+        """
+        lwr_bnd = self.con_conv - self._sd_con * self._z_score
+        upr_bnd = self.con_conv + self._sd_con * self._z_score
+        return lwr_bnd, upr_bnd
---
+++ test/test_abtesting.py
@@ -0,0 +1,28 @@
+# from dev import AnalyticTesting
+import pytest
+import pandas as pd
+import numpy as np
+from scipy import stats
+import os
+
+os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+class TestAnalyticTesting:
+    @classmethod
+    def setup_class(cls):
+        cls.data = pd.read_csv("./data/statistical_significance_data.csv")
+        cls.p_null = cls.data["click"].mean()
+        cls.n_control = len(cls.data[cls.data["condition"] == 0])
+        cls.p_control = cls.data.loc[cls.data["condition"] == 0, "click"].mean()
+        cls.n_test = len(cls.data[cls.data["condition"] == 1])
+        cls.p_test = cls.data.loc[cls.data["condition"] == 1, "click"].mean()
+
+    def test_pvalue(self):
+        se_p = np.sqrt(
+            self.p_null * (1 - self.p_null) * (1 / self.n_control + 1 / self.n_test)
+        )
+        z = (self.p_test - self.p_control) / se_p
+        pvalue = 1 - stats.norm.cdf(z)
+        print(pvalue)
+        assert pvalue == pytest.approx(0.0394428219746)
